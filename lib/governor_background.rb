840f2dec8fa4be440b5b8500e8a0444ce3a955c2
Liam Morley <lmorley@mitre.org>
2011-04-20 01:27:34 -0400

    added basic 'run_in_background' to Article, doesn't do anything yet

--- lib/governor_background.rb
+++ lib/governor_background.rb
@@ -1,1 +1,14 @@
-require 'governor_background/plugin'
+background = Governor::Plugin.new('background')
+
+background.register_model_callback do |base|
+  module InstanceMethods
+    def run_in_background
+      puts 'about to yield'
+      yield
+      puts 'done yielding'
+    end
+  end
+  base.send :include, InstanceMethods
+end
+
+Governor::PluginManager.register background

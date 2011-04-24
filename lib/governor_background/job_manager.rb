34360ca86373223c01e5f986a847f8d57f5e7838
Liam Morley <liam@carpeliam.com>
2011-04-23 22:20:10 -0400

    job manager can remove finished jobs

--- lib/governor_background/job_manager.rb
+++ lib/governor_background/job_manager.rb
@@ -1,5 +1,6 @@
 module GovernorBackground
   class JobManager
+    @@finished_statuses = %w(completed failed killed).freeze
     cattr_reader :jobs
     class << self
       @@jobs = []
@@ -9,6 +10,12 @@ module GovernorBackground
       
       def clean(time = 1.day.ago)
         @@jobs.reject!{|j| j.created_at < time}
+      end
+      
+      def finished_jobs
+        finished_jobs = @@jobs.select{|j| @@finished_statuses.include? j.status }
+        @@jobs -= finished_jobs
+        return finished_jobs
       end
     end
   end
--- spec/governor_background/job_manager_spec.rb
+++ spec/governor_background/job_manager_spec.rb
@@ -28,5 +28,17 @@ module GovernorBackground
       JobManager.clean(2.hours.ago)
       JobManager.jobs.size.should == 1
     end
+    
+    it "will return all finished jobs and remove them from the list" do
+      JobManager.add(queued    = mock('queued',    :status => 'queued'))
+      JobManager.add(working   = mock('working',   :status => 'working'))
+      JobManager.add(completed = mock('completed', :status => 'completed'))
+      JobManager.add(failed    = mock('failed',    :status => 'failed'))
+      JobManager.add(killed    = mock('killed',    :status => 'killed'))
+      
+      JobManager.jobs.size.should == 5
+      JobManager.finished_jobs.should == [completed, failed, killed]
+      JobManager.jobs.size.should == 2
+    end
   end
 end
